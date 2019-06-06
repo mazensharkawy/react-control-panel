7a2a71531b94211848e093f1fd491d4ec4749ab2
Mazen Sharkawy <mazenmaher1997@gmail.com>
2019-06-07 01:19:20 +0200

    change table menu to side bar ; add styled-components & lodash

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,35 +1,32 @@
 import React from 'react';
 import { Menu, Popover, Position, toaster, Button, Icon } from 'evergreen-ui'
-import SelectedTable from './selectedtable.js'
-const Sidebar = () => {
+// import SelectedTable from './selectedtable.js'
+import _ from 'lodash'
+import styled from 'styled-components'
+
+const Table = styled.div`
+align-text: center;
+background-color: white;
+height: 50px;
+margin: 0px;
+&:hover {
+    background-color: palevioletred;
+    color: white;
+  }
+`
+const TableName = styled.p`
+margin: 0px;
+padding-top: 15px;
+text-align: center
+`
+
+const Sidebar = ({tables, selectTable = ()=>{}}) => {
     return (
         <div className='sidenav'>
-            <Popover
-                position={Position.BOTTOM_LEFT}
-                content={
-                    <Menu>
-                        <Menu.Group>
-                            <Menu.Item
-                                onSelect={() => <SelectedTable/> }
-                            >
-                               table 1
-                            </Menu.Item>
-                            <Menu.Item
-                                onSelect={() => toaster.notify('Share')}
-                            >
-                               table 2
-                            </Menu.Item>
-                            <Menu.Item
-                                onSelect={() => toaster.notify('Share')}
-                            >
-                               table 3
-                            </Menu.Item>
-                        </Menu.Group>
-                    </Menu>
-                }
-            >
-                <Button marginRight={16}>Tables <Icon icon='chevron-down' /></Button>
-            </Popover>
+        {_.map(tables, table => 
+            <Table onClick={()=>selectTable(table)}>
+                <TableName align="center">{table}</TableName>
+            </Table>)}
         </div>
     );
 }
